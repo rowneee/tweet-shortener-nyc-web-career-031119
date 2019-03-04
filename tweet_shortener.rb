4ac10e0eb23699317f7885efe732355f10e4eee9
Roni Shabo <roni.shabo@gmail.com>
2019-03-04 14:04:57 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,7 +16,7 @@ end
 def word_substituter(tweet)
     string = tweet.split.map do |word|
         if dictionary.keys.include?(word.downcase)
-                word = dictionary[word.downcase]
+            word = dictionary[word.downcase]
         else
             word
         end
