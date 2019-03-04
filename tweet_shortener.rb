1b5084d933fed8f154c4fb46308812d83e0094f9
Roni Shabo <roni.shabo@gmail.com>
2019-03-04 14:11:23 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,42 @@
-# Write your code here.
+def dictionary
+   {
+    "hello" => "hi", 
+    "to" => "2",
+    "too" => "2",
+    "two" => "2",
+    "for" => "4", 
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+     "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+tweet.split.collect {|word| dictionary.keys.include?(word.downcase) ? word = dictionary[word.downcase] : word }.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+  array.each do |element|
+    puts word_substituter(element)
+  end
+end 
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  new_tweet = ""
+  if word_substituter(tweet).length > 140 
+    new_tweet << word_substituter(tweet)[1..137] + "..."
+  else
+    new_tweet << word_substituter(tweet)
+  end
+  new_tweet
+end
