26256ab2d5bb0ca6b117df99e306dc6c91eadf30
Roni Shabo <roni.shabo@gmail.com>
2019-03-04 14:08:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,16 +1,13 @@
 def dictionary
   {
-    "hello" => "hi",
-    "to" => "2",
-    "two" => "2",
-    "too" => "2",
-    "for" => "4",
-    "four" => "4",
-    "be" => "b",
-    "you" => "u",
-    "at" => "@",
-    "and" => "&"
-  }
+ "hello" => 'hi',
+["to, two, too"] => '2',
+["for, four"] => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@" ,
+"and" => "&"
+    }
 end
 
 def word_substituter(tweet)
